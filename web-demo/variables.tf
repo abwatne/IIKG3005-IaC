bdfd7cb374a8c3698d8bbbc180e1ea240fcb1faa
abwatne <abwatne@stud.ntnu.no>
2024-10-10 17:29:05 +0200

    hurlumhei

---
+++ web-demo/variables.tf
@@ -0,0 +1,24 @@
+variable "rg_name" {
+  type        = string
+  description = "Name of rg"
+}
+
+variable "location" {
+  type        = string
+  description = "Name of location"
+}
+
+variable "sa_name" {
+  type        = string
+  description = "Name of SA"
+}
+
+variable "index_document" {
+  type        = string
+  description = "Name of index doc"
+}
+
+variable "source_content" {
+  type        = string
+  description = "Source for content for index/html side"
+}
