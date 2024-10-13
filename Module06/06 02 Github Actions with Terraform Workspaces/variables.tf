574174352fdf557c8b639478795592b3438aae96
abwatne <abwatne@stud.ntnu.no>
2024-10-13 06:30:07 +0200

    destroy

---
+++ Module06/06 02 Github Actions with Terraform Workspaces/variables.tf
@@ -0,0 +1,29 @@
+variable "rg_name" {
+  type        = string
+  description = "Name of rg"
+  default = "abswtest"
+}
+
+variable "location" {
+  type        = string
+  description = "Name of location"
+  default = "westeurope"
+}
+
+variable "sa_name" {
+  type        = string
+  description = "Name of SA"
+  default = "abswtest"
+}
+
+variable "index_document" {
+  type        = string
+  description = "Name of index doc"
+  default = "index.html"
+}
+
+variable "source_content" {
+  type        = string
+  description = "Source for content for index/html side"
+  default     = "<h1>test</h1>"
+}
